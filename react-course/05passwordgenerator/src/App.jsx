1ab88d13fdac399426ea662ad77ef428ff3da965
Sandeep Saimon <sandeeprajsaimon999@gmail.com>
2025-03-07 14:47:01 +0530

    passsword generator

---
+++ react-course/05passwordgenerator/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useCallback } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [useNumber, setUseNumber] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState('')
+  return (
+    <>
+      <div className='w-full max-w-wd mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white' placeholder='password' />
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range" min={8} max={100} value={length} className='cursor-pointer' onChange={(e) => setLength(e.target.value)} />
+            <label htmlFor="length">Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" defaultChecked={useNumber} onChange={() => setUseNumber((prev) => !prev)}/>
+            <label htmlFor="numbers">Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox" defaultChecked={charAllowed} onChange={() => setCharAllowed((prev) => !prev)}/>
+            <label htmlFor="characters">characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
