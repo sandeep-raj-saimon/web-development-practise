653abc7dbb50039bc42eb780e0eb323f589df0e4
Sandeep Saimon <sandeeprajsaimon999@gmail.com>
2025-03-07 17:03:28 +0530

    chrome extension changes

--- react-course/05passwordgenerator/src/App.jsx
+++ react-course/05passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,13 +8,24 @@ function App() {
   const [useNumber, setUseNumber] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState('')
+  const passwordRef = useRef(null)
+  const generatePassword = useCallback(() => {
+    setPassword('Raj')
+  }, [length, useNumber, charAllowed])
+  useEffect(() => {
+    generatePassword()
+  }, [length, useNumber, charAllowed])
+  const copyPassword = () => {
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select()
+  }
   return (
     <>
       <div className='w-full max-w-wd mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white' placeholder='password' />
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+          <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white' placeholder='password' ref={passwordRef} />
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer' onClick={copyPassword}>Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
--- react-course/05passwordgenerator/vite.config.js
+++ react-course/05passwordgenerator/vite.config.js
@@ -5,4 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // We'll output everything into "dist"
+    outDir: 'dist',
+    rollupOptions: {
+      // Since we want to use the extension as a popup, treat index.html as the entry point.
+      input: {
+        popup: 'index.html'
+      },
+      output: {
+        // We'll keep the same name for convenience
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
